38989810d8874ac702cb071d0b67bfed02afa2c1
Dilshika <30935988+Dilshika@users.noreply.github.com>
2021-02-14 13:40:22 +0530

    Delete 013_single_signin directory

--- 013_single_signin/profileservice/src/main/java/com/dil/cafecloud/profileservice/repository/CustomerRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dil.cafecloud.profileservice.repository;
-
-import com.dil.cafecloud.commons.model.Customer;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CustomerRepository  extends JpaRepository<Customer,Integer> {
-}
