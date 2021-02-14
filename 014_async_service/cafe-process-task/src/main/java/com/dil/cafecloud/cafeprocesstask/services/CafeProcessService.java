a9e854b3c13e1106632fecc1cc1417b04b1afb8b
Dilshika <30935988+Dilshika@users.noreply.github.com>
2021-02-14 13:46:05 +0530

    Delete 014_async_service/cafe-process-task directory

--- 014_async_service/cafe-process-task/src/main/java/com/dil/cafecloud/cafeprocesstask/services/CafeProcessService.java
+++
@@ -1,8 +0,0 @@
-package com.dil.cafecloud.cafeprocesstask.services;
-
-import org.springframework.stereotype.Service;
-
-@Service
-public interface CafeProcessService {
-    boolean validatePhNumber(String phNumber);
-}
