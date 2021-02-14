4995d282cdf9f5ee5e2f83a3895a61c639c1da4d
Dilshika <30935988+Dilshika@users.noreply.github.com>
2021-02-14 13:47:11 +0530

    Delete 017_service_discovery_with_discover directory

--- 017_service_discovery_with_discover/coffee-service/src/main/java/com/dil/cafecloud/coffeeservice/repository/CoffeeRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dil.cafecloud.coffeeservice.repository;
-
-import com.dil.cafecloud.model.coffee.Coffee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoffeeRepository  extends JpaRepository<Coffee,Integer> {
-}
