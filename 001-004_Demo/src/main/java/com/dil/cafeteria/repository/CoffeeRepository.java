c70d2c1f8642886854d73139e4fbcebbacb500b0
Dilshika <30935988+Dilshika@users.noreply.github.com>
2021-02-14 13:38:20 +0530

    Delete 001-004_Demo directory

--- 001-004_Demo/src/main/java/com/dil/cafeteria/repository/CoffeeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dil.cafeteria.repository;
-
-import com.dil.cafeteria.model.Coffee;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CoffeeRepository extends JpaRepository<Coffee,Integer> {
-
-}
