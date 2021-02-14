521ef54626c47de4ea05fd1ef50d26d3a669cc26
Dilshika <30935988+Dilshika@users.noreply.github.com>
2021-02-14 13:47:00 +0530

    Delete 016_service_discovery directory

--- 016_service_discovery/order-service/src/main/java/com/dil/cafecloud/orderservice/repository/OrderRepository.java
+++
@@ -1,7 +0,0 @@
-package com.dil.cafecloud.orderservice.repository;
-
-import com.dil.cafecloud.model.order.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order,Integer> {
-}
